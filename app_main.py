47498a0925730aa09544ead343e595a26540c316
Paweł Walkowiak <pawel.walkowiak@pwr.edu.pl>
2024-04-03 16:19:49 +0200

    Train model

--- app_main.py
+++ app_main.py
@@ -5,6 +5,8 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.logserver import configure_logger
 from app.routers import biom_router
+from app.router_utils import facever
+
 
 configure_logger()
 log = logging.getLogger(__name__)
@@ -26,3 +28,13 @@ app.include_router(biom_router.router)
 async def root():
     """Endpoint for checking if api is alive."""
     return {"message": "Alive"}
+
+
+@app.on_event("startup")
+def train_facever():
+    """Train facever on startup."""
+    log.info("Training facever.")
+    facever.train(
+        "/home/bwalkow/repos/biometry_face/datasets/processed_celeb_subset/train"
+    )
+    log.info("Facever training complete.")
